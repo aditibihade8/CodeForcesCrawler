2cd4923caa47f193e6f3fe3c2c78722d32516662
Shashank <thatsmyjam99@gmail.com>
2020-08-09 19:15:40 +0530

    Added Fusion Charts

--- user_accounts/forms.py
+++ user_accounts/forms.py
@@ -1,22 +1,17 @@
-# from django import forms
-# from django.contrib.auth.models import User
-# from django.contrib.auth.forms import UserCreationForm
-
-# class UserForm(UserCreationForm) :
-# 	cf_handle = forms.CharField(help_text = 'Required. Provide valid CodeForces Handle')
-# 	class Meta() :
-# 		model = User
-# 		fields = ('username','password','password2','cf_handle')
-
 from django import forms
-#from django.models import models
-from django.contrib.auth import login, authenticate
-from django.contrib.auth.forms import UserCreationForm
+from .models import UserProfileInfo
 from django.contrib.auth.models import User
 
-class RegisterForm(UserCreationForm):
-	cf_handle = forms.CharField(help_text = 'Required. Provide valid CodeForces Handle')
+class UserForm(forms.ModelForm):
+    password = forms.CharField(widget=forms.PasswordInput())
+    class Meta():
+        model = User
+        fields = ('username','password','email')
 
-	class Meta():
-		model = User
-		fields = ('username','password1', 'password2', 'cf_handle')
+class UserProfileInfoForm(forms.ModelForm):
+     class Meta():
+         model = UserProfileInfo
+         fields = ('cf_handle','profile_pic')
+
+class SearchHandle(forms.Form):
+    cf_handle = forms.CharField(label="Search Handle", max_length=35)
